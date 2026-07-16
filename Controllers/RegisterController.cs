c0e203074cb55855019b5f24803a6cf3ed3962bd
Marvenskie <ccs.marvenjames@gmail.com>
2026-07-16 16:32:13 +0800

    meowwww

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace INFASS_BAS.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string firstName, string lastName, string email, string password, string confirmPassword)
+        {
+            if (password != confirmPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Passwords do not match.");
+                return View();
+            }
+
+            // Add your database saving / registration logic here (e.g., ASP.NET Core Identity)
+
+            // Redirect to login page upon successful signup
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Views/Register/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace INFASS_BAS.Views.Register
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
