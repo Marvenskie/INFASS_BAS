e39bb1ce309628285a06455949d7b6e065d98d65
AnnNavz <css.annshirleynavasca@gmail.com>
2026-07-18 22:57:03 +0800

    Reapply "hatdog nimo lubi"
    This reverts commit 96213e67bf39bc9e1cea399c569d12e29dc919f8.

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace INFASS_BAS.Models
+{
+	public class User
+	{
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public string Username { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
+		public string ConfirmPassword { get; set; } = string.Empty;
+	}
+}
